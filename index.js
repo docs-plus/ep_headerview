1919a1ba9e6e5fb00b64c2ae77a20102078e4b30
Hossein M <marzban98@gmail.com>
2021-07-13 10:02:49 +0430

    chore: lint

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
-'use strict';
+'use strict'
 
-const eejs = require('ep_etherpad-lite/node/eejs/');
-const packageJson = require('./package.json');
+const eejs = require('ep_etherpad-lite/node/eejs/')
+const packageJson = require('./package.json')
 
 exports.eejsBlock_editbarMenuRight = (hookName, args, cb) => {
-  args.content += eejs.require('ep_headerview/templates/editbarButtons.ejs');
-  return cb();
-};
-
+  args.content += eejs.require('ep_headerview/templates/editbarButtons.ejs')
+  return cb()
+}
 
 exports.clientVars = (hook, context, callback) => {
   const result = {
-    version: packageJson.version,
-  };
+    version: packageJson.version
+  }
 
-  return callback({headerView: result});
-};
+  return ({ headerView: result })
+}
 
-const db = require('./server/dbRepository');
+const db = require('./server/dbRepository')
 
 exports.socketio = (hookName, args, cb) => {
-  const io = args.io.of('/headerview');
+  const io = args.io.of('/headerview')
   io.on('connection', (socket) => {
-    socket.on('addNewFilter', async (padId, {filterId, filterUrl, filterName}, callback) => {
-      const key = `filters:${padId}:${filterId}`;
-      const newFilter = {filterId, filterUrl, filterName};
-      console.log('asdasd', key, newFilter);
+    socket.on('addNewFilter', async (padId, { filterId, filterUrl, filterName }, callback) => {
+      const key = `filters:${padId}:${filterId}`
+      const newFilter = { filterId, filterUrl, filterName }
+      console.log('asdasd', key, newFilter)
       await db.set(key, newFilter)
-          .catch((error) => {
-            console.error('[headerview]: ', error);
-            callback(false);
-          });
+        .catch((error) => {
+          console.error('[headerview]: ', error)
+          callback(false)
+        })
 
-      socket.broadcast.to(padId).emit('addNewFilter', {filterId, filterUrl, filterName});
+      socket.broadcast.to(padId).emit('addNewFilter', { filterId, filterUrl, filterName })
 
-      callback({filterId, filterUrl, filterName});
-    });
+      callback({ filterId, filterUrl, filterName })
+    })
 
-    socket.on('editeFilter', async (padId, {filterId, filterUrl, filterName}, callback) => {
-      const key = `filters:${padId}:${filterId}`;
-      const newFilter = {filterId, filterUrl, filterName};
+    socket.on('editeFilter', async (padId, { filterId, filterUrl, filterName }, callback) => {
+      const key = `filters:${padId}:${filterId}`
+      const newFilter = { filterId, filterUrl, filterName }
       await db.set(key, JSON.stringify(newFilter))
-          .catch((error) => {
-            console.error('[headerview]: ', error);
-            callback(false);
-          });
-      callback(true);
-    });
+        .catch((error) => {
+          console.error('[headerview]: ', error)
+          callback(false)
+        })
+      callback(true)
+    })
 
-    socket.on('removeFilter', async (padId, {filterId}, callback) => {
+    socket.on('removeFilter', async (padId, { filterId }, callback) => {
       await db.remove(`filters:${padId}:${filterId}`)
-          .catch((error) => {
-            console.error('[headerview]: ', error);
-            callback(false);
-          });
+        .catch((error) => {
+          console.error('[headerview]: ', error)
+          callback(false)
+        })
 
-      socket.broadcast.to(padId).emit('removeFilter', {filterId});
-      callback(true);
-    });
+      socket.broadcast.to(padId).emit('removeFilter', { filterId })
+      callback(true)
+    })
 
     socket.on('getFilterList', async (padId, callback) => {
-      socket.join(padId);
+      socket.join(padId)
       let filters = await db.getFilterList(`filters:${padId}`)
-          .catch((error) => {
-            console.error('[headerview]: ', error);
-            callback(false);
-          });
-      if (!filters) filters = [];
-      filters = filters.filter((x) => x != null);
-      callback(filters);
-    });
-  });
-};
+        .catch((error) => {
+          console.error('[headerview]: ', error)
+          callback(false)
+        })
+      if (!filters) filters = []
+      filters = filters.filter((x) => x != null)
+      callback(filters)
+    })
+  })
+}
--- server/dbRepository.js
+++ server/dbRepository.js
@@ -1,25 +1,25 @@
-const db = require('ep_etherpad-lite/node/db/DB');
+const db = require('ep_etherpad-lite/node/db/DB')
 
 exports.get = (key) => db.get(key).catch((error) => {
-  throw new Error(`[repository]: set has an error, ${error.message}`);
-});
+  throw new Error(`[repository]: set has an error, ${error.message}`)
+})
 
 exports.set = (key, value) => db.set(key, value).catch((error) => {
-  throw new Error(`[repository]: set has an error, ${error.message}`);
-});
+  throw new Error(`[repository]: set has an error, ${error.message}`)
+})
 
 exports.remove = (key, value) => db.remove(key).catch((error) => {
-  throw new Error(`[repository]: remove has an error, ${error.message}`);
-});
+  throw new Error(`[repository]: remove has an error, ${error.message}`)
+})
 
 exports.getFilterList = async (key) => {
-  const results = [];
+  const results = []
 
   const keys = await db.findKeys(`${key}:*`, null).catch((error) => {
-    throw new Error(`[repository]: getLatestId has an error,${error.message}`);
-  });
+    throw new Error(`[repository]: getLatestId has an error,${error.message}`)
+  })
 
-  keys.map((keyId) => results.push(db.get(keyId)));
+  keys.map((keyId) => results.push(db.get(keyId)))
 
-  return Promise.all(results);
-};
+  return Promise.all(results)
+}
